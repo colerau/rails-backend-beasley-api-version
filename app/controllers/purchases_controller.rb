93b4bbdbcb1c965b9ef60766504e115b73c263e2
Cole Rau <colevrau@gmail.com>
2021-12-28 21:04:21 -0800

    frontend redirects to checkout component after user clicks buy now button

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,22 @@
 class PurchasesController < ApplicationController
+  before_action :initialize_session
 
   def purchase 
-    purchase_params
-
-    product = purchase_params
+    session[:cart] = purchase_params
+    if session[:cart]
+      render json: {message: "success", cart: session[:cart]}
+    else 
+      render json: {message: "something went wrong"}
+    end
   end
 
   private
 
   def purchase_params
     params.require(:purchase).permit(:category, :scent, :color, :bodyButterWeight, :bodyButterPrice)
+  end
+
+  def initialize_session
+    session[:cart] ||= []
   end
 end
