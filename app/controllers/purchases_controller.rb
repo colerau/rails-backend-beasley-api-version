c328b12b10d1ac0e9604ed9b278eecfa6b5e0f92
Cole Rau <colevrau@gmail.com>
2021-12-31 03:15:32 -0800

    in middle of building object for square api call

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 require 'square'
+require 'securerandom'
 
 class PurchasesController < ApplicationController
   before_action :initialize_session
@@ -23,26 +24,87 @@ class PurchasesController < ApplicationController
     end
     if session[:cart][:bodyButterPrice]
       product_name_string += ", Price: #{session[:cart][:bodyButterPrice]}"
+      api_call_item_base_price = session[:cart][:bodyButterPrice].to_i * 100
     end
     if session[:cart][:price]
       product_name_string += ", Price: #{session[:cart][:price]}"
+      api_call_item_base_price = session[:cart][:price].to_i * 100
     end
+
+    tax = api_call_item_base_price * 0.1040
 
     # Create an instance of the API Client and initialize it with the credentials 
     # for the Square account whose assets you want to manage.
     client = Square::Client.new(
-      access_token: 'YOUR SANDBOX ACCESS TOKEN HERE',
+      access_token: ENV["SANDBOX_ACCESS_TOKEN"],
       environment: 'sandbox'
     )
 
-    
+    result = client.checkout.create_checkout(
+      location_id: ENV["SANDBOX_LOCATION_ID"],
+      body: {
+        idempotency_key: SecureRandom.uuid,
+        order: {
+          order: {
+            location_id: ENV["SANDBOX_LOCATION_ID"],
+            line_items: [
+              {
+                name: ,
+                quantity: "1",
+                base_price_money: {
+                  amount: api_call_item_base_price,
+                  currency: "USD"
+                }
+              }
+            ],
+            taxes: [
+              {
+                name: "Sales Tax",
+                percentage: "10.4",
+                applied_money: {
+                  amount: tax,
+                  currency: "USD"
+                },
+                scope: "ORDER"
+              }
+            ],
+            fulfillments: [
+              {
+                type: "SHIPMENT",
+                shipment_details: {
+                  recipient: {
+                    display_name: purchase_params[:customerName],
+                    address: {
+                      address_line_1: "8118 61st Ave NW",
+                      address_line_2: "123",
+                      locality: "Gig Harbor",
+                      administrative_district_level_1: "Washington",
+                      postal_code: "98332",
+                      country: "US"
+                    }
+                  }
+                }
+              }
+            ]
+          },
+          idempotency_key: "b9b65019-ecf9-4d83-a3bc-49b86f81a862"
+        },
+        merchant_support_email: "mistybeasley@beasleyscents.com"
+      }
+    )
+
+    if result.success?
+      puts result.data
+    elsif result.error?
+      warn result.errors
+    end
 
   end
 
   private
 
   def purchase_params
-    params.require(:purchase).permit(:category, :scent, :color, :bodyButterWeight, :bodyButterPrice, :price, :addressLine1, :addressLine2, :city, :state, :postalCode)
+    params.require(:purchase).permit(:category, :scent, :color, :bodyButterWeight, :bodyButterPrice, :price, :addressLine1, :addressLine2, :city, :state, :postalCode, :customerName)
   end
 
   def initialize_session
