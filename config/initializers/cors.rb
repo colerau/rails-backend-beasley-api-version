55e18b0827b59e489aef7c1eb0772b1a94f05d46
Cole Rau <colevrau@gmail.com>
2022-01-02 04:51:52 -0800

    change cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
  
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'beasleyscents.com'
+    origins 'beasleyscents.netlify.app'
     
     resource '*',
       headers: :any,
